9b44f6cfcca8278bd7462b9c825efae516a02430
Duc Minh <taducminh2004@gmail.com>
2022-03-29 21:40:45 +0700

    render products

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import Card from "./components/Card";
-import image from "./images/photo-1453728013993-6d66e9c9123a.jpg";
-import image_2 from "./images/lasy_logo.png";
-import { useState } from "react";
+import products from "./mock_data/products";
 import "./styles/App.css";
 
 function App() {
-  const [images, setImages] = useState([
-    {
-      image: image,
-      title: "Picture 1",
-    },
-    {
-      image: image_2,
-      title: "Picture 2",
-    },
-  ]);
-
-  const handleClick = () => {
-    setImages([
-      {
-        image: image,
-        title: "Picture 1",
-      },
-      {
-        image: image_2,
-        title: "Picture 2",
-      },
-    ]);
-    // setImages(currentImages => currentImages.push({//1 object chứa thông tin 1 ảnh}))
-  };
-
   return (
     <div className="flex">
-      {images.map((item, index) => (
-        <Card key={index} image={item.image} title={item.title} />
+      {products.map((item) => (
+        <Card key={item.id} image={item.image} title={item.title} />
       ))}
-      <button onClick={handleClick}>Click me!</button>
     </div>
   );
 }
